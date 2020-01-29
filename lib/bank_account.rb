4b178a6cb6b1927a1f13cab41a8de9e77557c844
kowalix007 <kowalix007@gmail.com>
2020-01-29 18:48:37 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,26 @@
 class BankAccount
+attr_accessor :balance, :status
+attr_reader :name
 
-end
+ def initialize (name)
+   @name =  name
+   @balance = 1000 
+   @status = "open"
+ end
+
+ def deposit (money)
+   @balance = @balance + money
+ end
+ 
+ def display_balance
+   "Your balance is $#{@balance}."
+ end 
+ 
+ def valid?
+  if @status = "open" && @balance > 0 
+    true 
+    else
+    false
+ end 
+ end 
+end 
