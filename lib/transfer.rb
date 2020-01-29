c8777a727c4975ca9379db4d433af921757043ab
kowalix007 <kowalix007@gmail.com>
2020-01-29 19:06:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,4 +12,12 @@ attr_accessor :sender, :receiver, :status, :amount
   @sender.valid? && @receiver.valid? ? true : false
  end 
  
+ def execute_transaction
+ 
+ end 
+ 
+ def reverse_transfer
+  
+ end 
+ 
 end
